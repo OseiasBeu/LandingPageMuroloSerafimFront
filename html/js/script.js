e3ad25322839920210ad4f76cac86945564b78c9
root <root@ip-172-31-30-90.us-east-2.compute.internal>
2020-10-17 18:22:07 +0000

    Commit central

---
+++ html/js/script.js
@@ -0,0 +1,54 @@
+let nome = document.getElementById("nomeid");
+let email = document.getElementById("emailid");
+let phone = document.getElementById("phoneid");
+let postBtn = document.getElementById("postBTN")
+
+
+const sendData = () => {
+    // verificar se o nome está vazio
+    if (nome.value == "") {
+      alert("Nome não informado");
+      // Deixa o input com o focus
+      nome.focus();
+      // retorna a função e não olha as outras linhas
+      return;
+    }
+  
+    if (email.value == "" || email.value.indexOf('@')==-1 || email.value.indexOf('.')==-1 ) {
+      alert("Preencha o campo de email corretamente");
+      email.focus();
+      return;
+    }
+    if (phone.value == "" || phone.value.length < 8) {
+      alert("O número de telefone está incorreto!");
+      phone.focus();
+      return;
+    }
+   // postBtn.href = 'https://chat.whatsapp.com/CCl6fKnBlPz50nphJaBzHR'
+    console.log('entrou')
+    // validar()
+    axios
+      .post(
+        'http://3.128.155.116:3001',
+        {
+          nome: nome.value,
+          email: email.value,
+          phne: phone.value
+        },
+        {
+          // headers: {
+          //   'Content-Type': 'application/json'
+          // }
+        }
+      )
+      .then(response => {
+        console.log(response);
+        
+      })
+      .catch(err => {
+        console.log(err, err.response);
+      });
+  };
+  
+ 
+postBtn.addEventListener('click', sendData);
